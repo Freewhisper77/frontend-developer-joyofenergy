e8174bd02a741be3f6c55cc524fc109a5a08ee31
Freewhisper77 <emmasddx@foxmail.com>
2022-11-08 11:29:35 +0800

    feat: [JOI-2]calculate the power consumption of last 30 days and display in the box

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ import { getReadings, groupByDay, sortByTime } from "./reading";
 
 const readings = await getReadings();
 renderChart(sortByTime(groupByDay(readings)).slice(-30));
+
+// calculate values -> power-consumption(substitute 895)
+const getConsumption = (readings) =>{
+    const values = readings.map(({ value }) => value);
+    return Math.round(values.reduce((x, y) => x + y));
+}
+
+const result = getConsumption(sortByTime(groupByDay(readings)).slice(-30));
+
+document.getElementById("getConsumption").innerHTML = result;
