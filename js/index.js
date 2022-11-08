7094b2a7cce5c942957b6906bc85f1261ddea3d1
Freewhisper77 <emmasddx@foxmail.com>
2022-11-08 14:05:10 +0800

    refactor: [JOY-2]create a new document for box

--- js/index.js
+++ js/index.js
@@ -2,16 +2,6 @@ import { renderChart } from "./chart.js";
 import { getReadings, groupByDay, sortByTime } from "./reading";
 
 const readings = await getReadings();
-const data = sortByTime(groupByDay(readings)).slice(-30);
+export const data = sortByTime(groupByDay(readings)).slice(-30);
 
 renderChart(data);
-
-// calculate values -> power-consumption(substitute 895)
-const getConsumption = (readings) =>{
-    const values = readings.map(({ value }) => value);
-    return Math.round(values.reduce((x, y) => x + y));
-}
-
-const powerConsumption = getConsumption(data);
-
-document.getElementById("powerConsumption").innerHTML = powerConsumption.toString();
