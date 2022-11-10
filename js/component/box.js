59a1e477b4bd19dc992720afed8326bcc42f7c57
Freewhisper77 <emmasddx@foxmail.com>
2022-11-10 11:05:17 +0800

    refactor: [JOI-3] add function getCost & getFootPrint

--- js/component/box.js
+++ js/component/box.js
@@ -1,16 +1,24 @@
-export const getConsumption = (readings) => {
+export const getPowerConsumption = (readings) => {
   const values = readings.map(({ value }) => value);
-  const powerConsumption = Math.round(values.reduce((x, y) => x + y));
-  const cost = Math.round(powerConsumption * 0.138);
-  let footprint = (powerConsumption * 0.0002532).toFixed(4);
-  return [cost, powerConsumption, footprint];
+  return Math.round(values.reduce((x, y) => x + y));
+};
+
+export const getCost = (readings) => {
+  const consumption = getPowerConsumption(readings);
+  return Math.round(consumption * 0.138);
+};
+
+export const getFootPrint = (readings) => {
+  const consumption = getPowerConsumption(readings);
+  return (consumption * 0.0002532).toFixed(4);
 };
 
 export const renderBox = (data) => {
-  const Consumption = getConsumption(data);
+  const consumption = getPowerConsumption(data);
+  const cost = getCost(data);
+  const footprint = getFootPrint(data);
 
-  document.getElementById("cost").innerHTML = Consumption[0].toString();
-  document.getElementById("powerConsumption").innerHTML =
-    Consumption[1].toString();
-  document.getElementById("footprint").innerHTML = Consumption[2].toString();
+  document.getElementById("cost").innerHTML = cost.toString();
+  document.getElementById("powerConsumption").innerHTML = consumption.toString();
+  document.getElementById("footprint").innerHTML = footprint;
 };
