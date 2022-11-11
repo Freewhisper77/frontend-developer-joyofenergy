ecad60fe343816480b805d2472aefdfe5aec7547
Freewhisper77 <emmasddx@foxmail.com>
2022-11-11 09:08:07 +0800

    refactor: [JOI-3] change the arguments passed to the getCost & getFootPrint

--- js/component/box.js
+++ js/component/box.js
@@ -3,20 +3,18 @@ export const getPowerConsumption = (readings) => {
   return Math.round(values.reduce((x, y) => x + y));
 };
 
-export const getCost = (readings) => {
-  const consumption = getPowerConsumption(readings);
+export const getCost = (consumption) => {
   return Math.round(consumption * 0.138);
 };
 
-export const getFootPrint = (readings) => {
-  const consumption = getPowerConsumption(readings);
+export const getFootPrint = (consumption) => {
   return (consumption * 0.0002532).toFixed(4);
 };
 
 export const renderBox = (data) => {
   const consumption = getPowerConsumption(data);
-  const cost = getCost(data);
-  const footprint = getFootPrint(data);
+  const cost = getCost(consumption);
+  const footprint = getFootPrint(consumption);
 
   document.getElementById("cost").innerHTML = cost.toString();
   document.getElementById("powerConsumption").innerHTML = consumption.toString();
