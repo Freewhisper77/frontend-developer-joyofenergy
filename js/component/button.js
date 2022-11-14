154284a4c235fd2eaed216267191486cb80c2a2d
Freewhisper77 <emmasddx@foxmail.com>
2022-11-14 12:57:35 +0800

    feat: [JOI-4] add interaction with last-day button

---
+++ js/component/button.js
@@ -0,0 +1,6 @@
+const unClickBtn = document.getElementById("last-day");
+
+unClickBtn.onclick = function () {
+  unClickBtn.style.background = "rgb(255, 255, 255)";
+  unClickBtn.style.color = "rgb(109, 142, 207)";
+}
